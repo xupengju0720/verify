d8818701382b21cefe0c743a9025549d5642abfd
xupengju <2399789174@qq.com>
2020-12-07 19:07:18 +0800

    使用 easyexcel处理excel表格 代码抽象封装
    对比数据  主要思路是
    1.读取数据
    2.准备对比数据
    3.数据对比
    4.数据写出

--- src/main/java/com/daily/verify/verify/excel/easyexcel/service/BaseService.java
+++ src/main/java/com/daily/verify/verify/excel/easyexcel/service/BaseService.java
@@ -8,17 +8,33 @@ import java.util.List;
 import java.util.Map;
 
 public interface BaseService<E, P> {
+    /**
+     *判断文件是否存在并且获取问价
+     */
     File rule(String filePath);
 
+    /**
+     *主要流程方法
+     */
     ApiResponse ExcelHandel(String funcId, String groupId, E e, P p);
-
+    /**
+     *读取excel的参数Sheet
+     */
     List<P> readParamExcel(File file, Class<P> tClass, String sheetName);
-
+    /**
+     *读取excel的预期值
+     */
     List<E> readExpireExcel(File file, Class<E> tClass, String sheetName);
-
+    /**
+     *使用参数值 来准备对比值 由各自的实例自己实现
+     */
     List<E> readyData(List<P> pList);
-
+    /**
+     *进行数据对比
+     */
     Map<String, List<Map<String, Object>>> compareExcel(List<E> listE, List<E> listD);
-
+    /**
+     *对比结果写入excel并输出 兼容xls额xlsx
+     */
     void writeExcel(Map<String, List<Map<String, Object>>> resultMap, Workbook workbook, String fileType, String sheetName, int sheetIndex, E e);
 }
